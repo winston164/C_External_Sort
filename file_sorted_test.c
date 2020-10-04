6615d4d93a815b80abc3130c5106fdd88dd9aa7c
Winston <winstonmartinbendana@gmail.com>
2020-10-05 02:09:30 +0800

    Fixed test

--- file_sorted_test.c
+++ file_sorted_test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
 
 int main(int argc, char const *argv[])
 {
@@ -7,7 +9,7 @@ int main(int argc, char const *argv[])
 
     int inputNum = INT_MIN, prev =INT_MIN;
 
-    while(fscanf(testFile, "%d", &inputNum)){
+    while(fscanf(testFile, "%d", &inputNum) == 1){
         if(inputNum < prev){
             printf("Wrong sorting at %d", inputNum);
         }
